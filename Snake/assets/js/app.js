70dcbfe22f54e383b9c4dac83be7fecd9aa7b0a9
Jonybang <Jonybange@gmail.com>
2014-10-29 23:04:23 +1100

    scroll to 1 level, nad disable scroll

--- Snake/assets/js/app.js
+++ Snake/assets/js/app.js
@@ -3,5 +3,48 @@ define([
 	"./helpers/helpers",
 	"domReady!"
   ], function(test, helpers) {
-    return test;
+
+	$('html, body').animate({
+		scrollTop: $('#level_5').offset().top
+	}, 0);
+	var keys = [37, 38, 39, 40];
+
+	function preventDefault(e) {
+		e = e || window.event;
+		if (e.preventDefault)
+			e.preventDefault();
+		e.returnValue = false;
+	}
+
+	function keydown(e) {
+		for (var i = keys.length; i--;) {
+			if (e.keyCode === keys[i]) {
+				preventDefault(e);
+				return;
+			}
+		}
+	}
+
+	function wheel(e) {
+		preventDefault(e);
+	}
+
+	function disable_scroll() {
+		if (window.addEventListener) {
+			window.addEventListener('DOMMouseScroll', wheel, false);
+		}
+		window.onmousewheel = document.onmousewheel = wheel;
+		document.onkeydown = keydown;
+	}
+
+	disable_scroll();
+
+	function enable_scroll() {
+		if (window.removeEventListener) {
+			window.removeEventListener('DOMMouseScroll', wheel, false);
+		}
+		window.onmousewheel = document.onmousewheel = document.onkeydown = null;
+	}
+
+	return test;
 });
