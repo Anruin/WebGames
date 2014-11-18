500885686ba31188126ea27c5d833626e00cd4ae
Jonybang <Jonybange@gmail.com>
2014-11-18 20:38:14 +1000

    update background.js

--- Snake/assets/js/background.js
+++ Snake/assets/js/background.js
@@ -19,75 +19,38 @@
             function getWindowHeight() {
                 return windowHeight;
             }
+		
+			function initBackground(){
+				var controller = new ScrollMagic();
+			
+				// with scene reset
+				controller = controller.destroy(true);
+				controller = new ScrollMagic();
+				
+				var tween1, tween2, tween3, tween4, tween5, scene1, scene2, scene3, scene4, scene5;
 
+				tween1 = TweenMax.fromTo("#game_background", 3.5,
+					{"top": "-100%"},
+					{"top": "-1100%",
+					ease: Linear.easeNone, 
+					immediateRender:false
+					}
+				);
+				
+				scene1 = new ScrollScene({triggerElement: "#game_levels", offset: getWindowHeight() / 2 - 1, duration: getWindowHeight() * 5 })
+					.setTween(tween1)
+					.addTo(controller);
+			}
+
+			initBackground();
+			
             // update window height on resize
             $(window).on("resize", function () {
                 windowHeight = $(window).innerHeight();
+				initBackground();
             });
 
-
-
-            var controller = new ScrollMagic();
-            var tween1, tween2, tween3, tween4, tween5, scene1, scene2, scene3, scene4, scene5;
-
-
-            tween1 = TweenMax.fromTo("#game_background", 0.2,
-                {"top": "-100%", "width": "100%"},
-                {"top": "-290%", "width": "120%"},
-                {ease: Linear.easeNone}
-            );
-            tween2 = TweenMax.fromTo("#game_background", 0.2,
-                {"top": "-290%", "width": "120%"},
-                {"top": "-493%", "width": "100%"},
-                {ease: Linear.easeNone}
-            );
-            tween3 = TweenMax.fromTo("#game_background", 0.2,
-                {"top": "-493%"},
-                {"top": "-685%"},
-                {ease: Linear.easeNone}
-            );
-            tween4 = TweenMax.fromTo("#game_background", 0.2,
-                {"top": "-685%"},
-                {"top": "-890%"},
-                {ease: Linear.easeNone}
-            );
-            tween5 = TweenMax.fromTo("#game_background", 0.2,
-                {"top": "-890%"},
-                {"top": "-1100%"},
-                {ease: Linear.easeNone}
-            );
-
-            // build scene
-            scene5 = new ScrollScene({triggerElement: "#game_levels", offset: getWindowHeight() * 4 + getWindowHeight() / 2, duration: getWindowHeight() })
-                .setTween(tween5)
-                .addTo(controller);
-
-            // build scene
-            scene4 = new ScrollScene({triggerElement: "#game_levels", offset: getWindowHeight() * 3 + getWindowHeight() / 2, duration: getWindowHeight() })
-                .setTween(tween4)
-                .addTo(controller);
-
-            // build scene
-            scene3 = new ScrollScene({triggerElement: "#game_levels", offset: getWindowHeight() * 2 + getWindowHeight() / 2, duration: getWindowHeight() })
-                .setTween(tween3)
-                .addTo(controller);
-
-            // build scene
-            scene2 = new ScrollScene({triggerElement: "#game_levels", offset: getWindowHeight() + getWindowHeight() / 2, duration: getWindowHeight() })
-                .setTween(tween2)
-                .addTo(controller);
-
-            // build scene
-            scene1 = new ScrollScene({triggerElement: "#game_levels", offset: getWindowHeight() / 2 - 1, duration: getWindowHeight() })
-                .setTween(tween1)
-                .addTo(controller);
-
-            // show indicators (requires debug extension)
-            //scene1.addIndicators({zindex: 1000});
-            //scene2.addIndicators({zindex: 1000});
-            //scene3.addIndicators({zindex: 1000});
-            //scene4.addIndicators({zindex: 1000});
-            //scene5.addIndicators({zindex: 1000});
+            
         };
 
         // INIT
