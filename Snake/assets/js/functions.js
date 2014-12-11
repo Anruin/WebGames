0c01d63b60dcff5e87cfeabc42f2cb338ac957db
Jonybang <Jonybange@gmail.com>
2014-12-12 00:30:10 +1000

    add sounds

--- Snake/assets/js/functions.js
+++ Snake/assets/js/functions.js
@@ -103,3 +103,100 @@ function send() {
 		}
 	});
 }
+
+function BufferLoader(context, urlList, callback) {
+	this.context = context;
+	this.urlList = urlList;
+	this.onload = callback;
+	this.bufferList = new Array();
+	this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.responseType = "arraybuffer";
+	var loader = this;
+	request.onload = function() {
+		loader.context.decodeAudioData(
+				request.response,
+				function(buffer) {
+					if (!buffer) {
+						alert('error decoding file data: ' + url);
+						return;
+					}
+					loader.bufferList[index] = buffer;
+					if (++loader.loadCount == loader.urlList.length)
+						loader.onload(loader.bufferList);
+				},
+				function(error) {
+					console.error('decodeAudioData error', error);
+				}
+		);
+	}
+	request.onerror = function() {
+		alert('BufferLoader: XHR error');
+	}
+	request.send();
+}
+
+BufferLoader.prototype.load = function() {
+	for (var i = 0; i < this.urlList.length; ++i)
+		this.loadBuffer(this.urlList[i], i);
+}
+
+$(function() {
+	var BUFFERS = {};
+	var context = null;
+	var names = ["steps", "pit", "bang", "pickUp", "give"];
+	var BUFFERS_TO_LOAD = {};
+	names.map(function(name){
+		BUFFERS_TO_LOAD[name] = 'http://games.narleyn.ru/Snake/assets/sounds/' + name + '.mp3';
+	});
+
+	function loadBuffers() {
+		var names = [];
+		var paths = [];
+		for (var name in BUFFERS_TO_LOAD) {
+			var path = BUFFERS_TO_LOAD[name];
+			names.push(name);
+			paths.push(path);
+		}
+		var bufferLoader = new BufferLoader(context, paths, function (bufferList) {
+			for (var i = 0; i < bufferList.length; i++) {
+				var buffer = bufferList[i];
+				var name = names[i];
+				BUFFERS[name] = buffer;
+			}
+		});
+		bufferLoader.load();
+	}
+	function playSound(buffer, time) {
+		var source = context.createBufferSource();
+		source.buffer = buffer;
+		source.connect(context.destination);
+		if (!source.start)
+			source.start = source.noteOn;
+		source.start(time);
+	}
+	try {
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		window.context = new AudioContext();
+	}
+	catch(e) {
+		alert("Web Audio API is not supported in this browser");
+	}
+	loadBuffers();
+	$('.level-3__advantage.adv-1').click(function(){
+		playSound(BUFFERS.steps, 0);
+	});
+	$('.level-3__advantage.adv-2').click(function(){
+		playSound(BUFFERS.pit, 0);
+	});
+	$('.level-3__advantage.adv-3').click(function(){
+		playSound(BUFFERS.bang, 0);
+	});
+	$('.level-3__advantage.adv-4').click(function(){
+		playSound(BUFFERS.pickUp, 0);
+	});
+})
