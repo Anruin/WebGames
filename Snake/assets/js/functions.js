ae22629f5d3e119e26535ed3f371d34f5021e509
Jonybang <Jonybange@gmail.com>
2014-12-24 11:44:13 +1000

    fix timer

--- Snake/assets/js/functions.js
+++ Snake/assets/js/functions.js
@@ -51,17 +51,21 @@ $(document).ready(function() {
 		else
 			dayName = enducement(daysLeft, "день", "дня", "дней");
 
+		var $firstDigit = jQuery('.timer__digit:first');
+		var $lastDigit = jQuery('.timer__digit:last');
 		if (daysLeft < 0) {
 			$firstDigit.addClass('digit-0');
 			$lastDigit.addClass('digit-0');
 		} else {
-			var $firstDigit = jQuery('.timer__digit:first');
-			var $lastDigit = jQuery('.timer__digit:last');
 			var $days = jQuery('span.days > span');
 			if (daysLeft == 0) {
 				document.write("00");
 			} else {
-				$firstDigit.addClass('digit-' + daysLeftString.substr(daysLeftString.length - 2, 1));
+				if(daysLeft > 10)
+					$firstDigit.addClass('digit-' + daysLeftString.substr(daysLeftString.length - 2, 1));
+				else
+					$firstDigit.addClass('digit-0');
+				
 				$lastDigit.addClass('digit-' + daysLeftString.substr(daysLeftString.length - 1));
 				$days.html(dayName);
 			}
